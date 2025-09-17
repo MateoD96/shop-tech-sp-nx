eb40b84e3e670f45e4ec0f3ba0c735311c3fab8e
Mateo Castillo <mate.dev9618@gmail.com>
2025-09-17 17:17:24 -0500

    initial config

---
+++ src/lib/constants.ts
@@ -0,0 +1,20 @@
+/* export type Categories = "phones" | "laptops" | "Video Consolas";
+
+export const filters = [
+  {
+    category: "phones",
+    marca: ["Apple", "Samsung", "Xiaomi", "Huawei", "Generic"],
+    price: { min: 50, max: 2000 },
+  },
+  {
+    category: "laptops",
+    marca: ["Apple", "Dell", "HP", "Lenovo", "Asus", "Acer"],
+    price: { min: 200, max: 3000 },
+  },
+  {
+    category: "Video Consolas",
+    marca: ["Sony", "Microsoft", "Nintendo"],
+    price: { min: 100, max: 1000 },
+  },
+] as const;
+ */
